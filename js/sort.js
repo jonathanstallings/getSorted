bd2c4bccadc3f1e47b6d3d20b283fccf8ad1d9c5
Jonathan Stallings <jonathanstallings@outlook.com>
2015-02-27 12:36:55 -0800

    Working, but janky animation for swap and exchange.

--- js/sort.js
+++ js/sort.js
@@ -6,7 +6,7 @@ function Block (column) {
   this.toHTML = function () {
     var element = '<div class="blockFrame"><div class="blockItem ';
     element += this.itemHeight + '"';
-    element += ">" + column + "</div></div>"
+    element += ">" + column + "</div></div>";
     return element;
   };
 }
@@ -16,45 +16,72 @@ function Grid (columns) {
 
   for (var i = 1; i <= columns; i++) {
     var block = new Block(i);
-    $display.append(block.toHTML())
+    $display.append(block.toHTML());
   }
+}
+
+function selectBlock (block) {
+  //selects a DOM block
+  return $display.children().eq(block-1);
 }
 
 function swap (first, second) {
   //swap two blocks
-  $display.children().eq(second-1)
-    .after($display.children().eq(first-1))
-    .insertBefore($display.children().eq(first-1));
+  selectBlock(second)
+    .after(selectBlock(first))
+    .insertBefore(selectBlock(first));
 }
 
 function toggleRaise (block) {
   //raise a block
-  $display.children().eq(block - 1).toggleClass("raised");
+  // $display.children().eq(block - 1).toggleClass("raised");
+  selectBlock(block).toggleClass("raised");
+  return selectBlock(block);
 }
 
-function togglePush (block, direction) { //refactor with toggleRaise
+function toggleNudge (block, direction) { //refactor with toggleRaise
   //push a direction
-  $display.children().eq(block -1).toggleClass(direction);
+  selectBlock(block).toggleClass(direction);
+  return selectBlock(block);
 }
 
-function toggleFade (block) {
+function pushBlock (block, units) { //refactor with toggleRaise
+  //push a direction
+  var offset = 'shift';
+
+  if (units < 0) {
+    offset += "Left";
+  } else {
+    offset += "Right";
+  }
+  offset += Math.abs(units);
+  selectBlock(block)
+    // .toggleClass("showTransitions")
+    .toggleClass(offset)
+    .prev().toggleClass("rightSideGap");
+
+  return selectBlock(block);
+}
+
+function toggleHidden (block) {
   //toggles a fade
-  $display.children().eq(block-1).toggleClass("hidden");
+  selectBlock(block).toggleClass("hidden");
+  return selectBlock(block);
 }
 
 function exchange (first, second) { //fine tune this
   //exchange two blocks with animation
   toggleRaise(second);
-  togglePush(second, "left");
-  togglePush(first, "right")
+  toggleNudge(second, "left");
+  toggleNudge(first, "right");
   setTimeout(swap, 800, first, second);
-  setTimeout(togglePush, 1000, second, "right");
-  setTimeout(togglePush, 1000, first, "left");
+  setTimeout(toggleNudge, 1000, second, "right");
+  setTimeout(toggleNudge, 1000, first, "left");
   setTimeout(toggleRaise, 1000, first);
 }
 
 function doIt () {
-  //test 
+  //test
   exchange(9,10);
   setTimeout(exchange, 2000, 8, 9);
 }
