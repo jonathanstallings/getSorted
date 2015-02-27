c4706062362ad3d47dc09ccc33c0a2a7b25f93e9
Jonathan Stallings <jonathanstallings@outlook.com>
2015-02-26 20:31:17 -0800

    Got the contstructor working

--- js/sort.js
+++ js/sort.js
@@ -1,19 +1,37 @@
+ $(function () {
+   // body...
+  $display = $("#display");
 
-$div = $("#display");
-
-function Block () {
-  this.blockSize = blockSize;
-}
-
-var shuffle = function(m) {
-  var rand, $rand;
-  rand = Math.floor(Math.random() * m--);
-  $('li:eq(' + m + ')').
-    after($('li:eq(' + rand + ')')).
-    insertBefore($('li:eq(' + rand + ')'));
-  if(m) {
-    setTimeout(shuffle, 100, m);
+  function Block (column) {
+    this.itemHeight = "height" + column;
+    this.toHTML = function () {
+      var element = '<div class="blockFrame"><div class="blockItem ';
+      element += this.itemHeight + '"';
+      element += ">" + column + "</div></div>"
+      return element;
+    };
   }
-};
-var deck = new Deck();
-shuffle($('.cards').length);
+
+
+  function Grid (columns) {
+    this.blocks = [];
+
+    for (var i = 1; i <= columns; i++) {
+      var block = new Block(i);
+      $display.append(block.toHTML())
+    }
+  }
+  var grid = new Grid(10);
+ });
+
+// var shuffle = function(m) {
+//   var rand, $rand;
+//   rand = Math.floor(Math.random() * m--);
+//   $('li:eq(' + m + ')').
+//     after($('li:eq(' + rand + ')')).
+//     insertBefore($('li:eq(' + rand + ')'));
+//   if(m) {
+//     setTimeout(shuffle, 100, m);
+//   }
+// };
+
