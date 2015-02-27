d7652a0df0d9feb0bbd0f732005f7443402faaab
Jonathan Stallings <jonathanstallings@outlook.com>
2015-02-27 00:05:00 -0800

    Created working swap and toggleRaise functions for animation.

--- js/sort.js
+++ js/sort.js
@@ -1,28 +1,36 @@
- $(function () {
-   // body...
-  $display = $("#display");
 
-  function Block (column) {
-    this.itemHeight = "height" + column;
-    this.toHTML = function () {
-      var element = '<div class="blockFrame"><div class="blockItem ';
-      element += this.itemHeight + '"';
-      element += ">" + column + "</div></div>"
-      return element;
-    };
+$display = $("#display");
+
+function Block (column) {
+  this.itemHeight = "height" + column;
+  this.toHTML = function () {
+    var element = '<div class="blockFrame"><div class="blockItem ';
+    element += this.itemHeight + '"';
+    element += ">" + column + "</div></div>"
+    return element;
+  };
+}
+
+function Grid (columns) {
+  this.blocks = [];
+
+  for (var i = 1; i <= columns; i++) {
+    var block = new Block(i);
+    $display.append(block.toHTML())
   }
+}
 
+function swap (first, second) {
+  //swap two blocks
+  $display.children().eq(second-1).after($display.children().eq(first-1)).insertBefore($display.children().eq(first-1));
+}
 
-  function Grid (columns) {
-    this.blocks = [];
+function toggleRaise (block) {
+  //raise a block
+  $display.children().eq(block - 1).toggleClass("raised");
+}
 
-    for (var i = 1; i <= columns; i++) {
-      var block = new Block(i);
-      $display.append(block.toHTML())
-    }
-  }
-  var grid = new Grid(10);
- });
+var grid = new Grid(10);
 
 // var shuffle = function(m) {
 //   var rand, $rand;
