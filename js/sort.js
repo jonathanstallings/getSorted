e169d5557e5cb1a9655b52789c4eec8e71cfefbc
Jonathan Stallings <jonathanstallings@outlook.com>
2015-02-27 00:54:15 -0800

    Have the beginnings of an animation cycle.

--- js/sort.js
+++ js/sort.js
@@ -22,12 +22,41 @@ function Grid (columns) {
 
 function swap (first, second) {
   //swap two blocks
-  $display.children().eq(second-1).after($display.children().eq(first-1)).insertBefore($display.children().eq(first-1));
+  $display.children().eq(second-1)
+    .after($display.children().eq(first-1))
+    .insertBefore($display.children().eq(first-1));
 }
 
 function toggleRaise (block) {
   //raise a block
   $display.children().eq(block - 1).toggleClass("raised");
+}
+
+function togglePush (block, direction) { //refactor with toggleRaise
+  //push a direction
+  $display.children().eq(block -1).toggleClass(direction);
+}
+
+function toggleFade (block) {
+  //toggles a fade
+  $display.children().eq(block-1).toggleClass("hidden");
+}
+
+function exchange (first, second) { //fine tune this
+  //exchange two blocks with animation
+  toggleRaise(second);
+  togglePush(second, "left");
+  togglePush(first, "right")
+  setTimeout(swap, 800, first, second);
+  setTimeout(togglePush, 1000, second, "right");
+  setTimeout(togglePush, 1000, first, "left");
+  setTimeout(toggleRaise, 1000, first);
+}
+
+function doIt () {
+  //test 
+  exchange(9,10);
+  setTimeout(exchange, 2000, 8, 9);
 }
 
 var grid = new Grid(10);
